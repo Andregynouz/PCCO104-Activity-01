2871e00c3a8e9e78b64faffea2346d0ea6d6204e
lorenzoludwig6 <31803665-lorenzoludwig6@users.noreply.replit.com>
2024-02-03 02:44:28 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string fname = "Andre Robert";
+    Console.WriteLine ("Hello!"+ " " + fname);
+    Console.WriteLine ($"Hello! {fname}" );
+  }
+}
