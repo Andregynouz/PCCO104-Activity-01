2c91e4920fa8954beaaaab28b0fbcfb2b918f9c1
lorenzoludwig6 <31803665-lorenzoludwig6@users.noreply.replit.com>
2024-02-03 02:50:02 +0000

    Added Age number

--- main.cs
+++ main.cs
@@ -4,7 +4,8 @@ class Program {
   public static void Main (string[] args) {
     string fname = "Andre Robert";
     string Lname = "Lorenzo";
-    Console.WriteLine ("Hello!"+ " " + fname + " " + Lname);
-    Console.WriteLine ($"Hello! {fname} {Lname}" );
+    int age = 19;
+    Console.WriteLine ("Hello!"+ " " + fname + " " + Lname + " " + age);
+    Console.WriteLine ($"Hello! {fname} {Lname} {age}" );
   }
 }
